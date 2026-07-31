fbec373cd891d89c624c2e630cca8564860321f3
Caio Patricio <Furyous2024@gmail.com>
2026-07-31 15:00:16 -0300

    atualaca

--- sharedNatal.js
+++ sharedNatal.js
@@ -618,7 +618,7 @@ const AYANAMSA_VALUES = {
   Tropical: 0,
   IAU: 28.6888982207,
   IAU2: 29.12,
-  IAUZeroAries: 31.8153223264,
+  IAUZeroAries: 33.8581806828,
   MidpointJ2000: 31.2836,
   Lahiri: 24.19,
   FaganBradley: 24.97,
